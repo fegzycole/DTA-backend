35be0e6758a4df27ecd3e7f751ef6d33faeb5653
fegzycole <fergusoniyara@gmail.com>
2020-01-26 16:10:45 +0100

    Setup express server

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { config } from 'dotenv';
+
+config();
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
